5b676ff16010b5dbd94e10c0315bb5e2989c79c2
Talha459 <talhaxx2@hotmail.com>
2024-06-12 21:46:43 -0700

    my files

---
+++ latenight.js
@@ -0,0 +1,20 @@
+var BBQMENU;
+(function (BBQMENU) {
+    BBQMENU["CHICKEN_TIKKA"] = "chicken-tikka";
+    BBQMENU["BEHARI_BOTI"] = "behari-boti";
+    BBQMENU["GOLA_KABAB"] = "gola-kabab";
+    BBQMENU["SEEKH_KABAB"] = "seekh-kabab";
+    BBQMENU["BEEF_BOTI"] = "beef-boti";
+})(BBQMENU || (BBQMENU = {}));
+var REVIEW;
+(function (REVIEW) {
+    REVIEW[REVIEW["GOOD"] = 0] = "GOOD";
+    REVIEW[REVIEW["BAD"] = 1] = "BAD";
+    REVIEW[REVIEW["DELICIOUS"] = 2] = "DELICIOUS";
+})(REVIEW || (REVIEW = {}));
+var BBQMENUORDER = {
+    ORDER: BBQMENU.BEHARI_BOTI,
+    PAYMENT: 200,
+    REVIEW: REVIEW.DELICIOUS
+};
+console.log(BBQMENUORDER);
